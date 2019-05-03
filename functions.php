4063cfc196b9e0b4fa58c0b559457b3cc4fa26c5
stan wielga <svwielga4@gmail.com>
2019-05-02 20:57:45 -0700

    edit version remove

--- functions.php
+++ functions.php
@@ -17,8 +17,19 @@ function my_theme_enqueue_styles()
 function twentynineteen_posted_by()
 { }
 
-// remove wp version from head
+// remove version from head
 remove_action('wp_head', 'wp_generator');
 
 // remove version from rss
 add_filter('the_generator', '__return_empty_string');
+
+// remove version from scripts and styles
+function remove_version_scripts_styles($src)
+{
+    if (strpos($src, 'ver=')) {
+        $src = remove_query_arg('ver', $src);
+    }
+    return $src;
+}
+add_filter('style_loader_src', 'remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'remove_version_scripts_styles', 9999);
